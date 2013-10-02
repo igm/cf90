ae022d56a47d9625b41f1d671255e19a46ea55c4
Igor Mihalik <igor.mihalik@gmail.com>
2013-10-02 16:47:48 +0100

    added target.info command

---
+++ target_info.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func init() {
+	register(&Command{
+		group:  "Target",
+		name:   "target.info",
+		help:   "Show target information",
+		handle: target_info,
+	})
+}
+
+func target_info() {
+	target, err := c.SelectedTarget()
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := target.Info()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("")
+	fmt.Println("Name                   :", info.Name)
+	fmt.Println("Build                  :", info.Build)
+	fmt.Println("Support                :", info.Support)
+	fmt.Println("Version                :", info.Version)
+	fmt.Println("Description            :", info.Description)
+	fmt.Println("Authorization Endpoint :", info.AuthorizationEndpoint)
+	fmt.Println("API Version            :", info.ApiVersion)
+	fmt.Println("")
+}
