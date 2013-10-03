198e22d4b074673f36b3a8885aed7b955e622a0a
Igor Mihalik <igor.mihalik@gmail.com>
2013-10-03 23:09:57 +0100

    org list update

--- org.go
+++ org.go
@@ -8,6 +8,6 @@ import (
 type OrgList []cf.Organization
 
 func (o OrgList) Len() int            { return len(o) }
-func (o OrgList) Render(i int) string { return fmt.Sprintf("%s", o[i].Name) }
-func (o OrgList) Title() string       { return "Organizations" }
+func (o OrgList) Render(i int) string { return fmt.Sprintf("%-20s %s", o[i].Name, o[i].Domains) }
+func (o OrgList) Title() string       { return fmt.Sprintf("%-20s %s", "Org", "Domains") }
 func (o OrgList) Selection() string   { return "Select organization:" }
