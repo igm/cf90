3b8c0f0bd56282742be02f0422e90a08ca8c7c09
Igor Mihalik <igor.mihalik@gmail.com>
2013-10-04 12:43:37 +0100

    now also zip,war,... archives can be pushed

--- app_push.go
+++ app_push.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/igm/cf"
 	"log"
 	"os"
@@ -11,10 +12,10 @@ func init() {
 	register(&Command{
 		group: "Application",
 		name:  "app.push",
-		help:  "Push application",
+		help:  "Push application (directory or file archive)",
 		params: []Param{
 			Param{name: "name", desc: "Application name"},
-			Param{name: "path", desc: "Application path", defval: "."},
+			Param{name: "path", desc: "Application path, or path to a archive file to upload (zip, war,...)", defval: "."},
 		},
 		handle: app_push,
 	})
@@ -45,23 +46,39 @@ func app_push() {
 		appId = apps[i].Guid
 	}
 
-	var archetypes []*cf.Archetype
-	filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			file, err := os.Open(p)
-			if err != nil {
-				return err
-			}
-			norm, _ := filepath.Rel(path, p)
-			archetypes = append(archetypes, &cf.Archetype{
-				Name:   norm,
-				Reader: file,
-			})
-		}
-		return nil
-	})
-	err = target.AppPush(appId, archetypes)
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
+	}
+	defer file.Close()
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if stat.IsDir() {
+		var archetypes []*cf.Archetype
+		filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+			if !info.IsDir() {
+				file, err := os.Open(p)
+				if err != nil {
+					return err
+				}
+				fmt.Println("Adding file:", info.Name())
+				norm, _ := filepath.Rel(path, p)
+				archetypes = append(archetypes, &cf.Archetype{
+					Name:   norm,
+					Reader: file,
+				})
+			}
+			return nil
+		})
+		err = target.AppPush(appId, archetypes)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("Pushing file:", stat.Name())
+		err = target.AppPushArchive(appId, file)
 	}
 }
